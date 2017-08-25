7b395b9873653443d7c58ef89668c3d6b0d47711
raghuramgupta <raghu_13_sa@yahoo.com>
2017-08-26 00:17:57 +0800

    Update the committed code

---
+++ Test-CI/src/Test.java
@@ -0,0 +1,24 @@
+import java.io.*;
+public class Test {
+
+	public static void main(String[] args) throws IOException
+	{
+		File f = new File("C:\\HDD\\Tools","Hello.txt");
+	      
+			FileWriter wr = new FileWriter(f,true);
+			wr.write(100);
+			wr.close();
+			System.out.println(f.length());
+			FileReader fr = new FileReader(f);
+			char[] a = new char[50];
+			fr.read();
+			String[] b = new String[1000];
+			int i=0;
+			for (char c:a)
+				{
+				System.out.println("Test the GIT"+c);
+				}
+			fr.close();
+		
+	}
+}
